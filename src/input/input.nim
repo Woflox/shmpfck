6bf050c931f875b3fbb8d469889ba7c41719ea68
woflox <eric.billingsley.1@gmail.com>
2015-02-09 16:14:31 -0500

    WIP input handling

--- src/input/input.nim
+++ src/input/input.nim
@@ -1,7 +1,22 @@
 import sdl2
 import ../util/util
+import tables
+
+type
+  keyConfig = object
+    key: string
 
 
-
+var keyMap = {"left"  : keyConfig(key: "Left"),
+              "right" : keyConfig(key: "Right"),
+              "up"    : keyConfig(key: "Up"),
+              "down"  : keyConfig(key: "Down"),
+              "select": keyConfig(key: "Return"),
+              "pause" : keyConfig(key: "Escape"),
+              "fire1" : keyConfig(key: "z"),
+              "fire2" : keyConfig(key: "c"),
+              "fire3" : keyConfig(key: "s"),
+              "fire4" : keyConfig(key: "x")}
 
 proc update(dt: float) =
+  discard
