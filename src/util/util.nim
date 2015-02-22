914b0cdb079e8727e0b02d7785221d7518756d94
woflox <eric.billingsley.1@gmail.com>
2015-02-22 17:36:04 -0500

    Added vector divide by float operator, made util style consistent

--- src/util/util.nim
+++ src/util/util.nim
@@ -7,52 +7,62 @@ type
     r*, g*, b*, a*: float
   Matrix2x2* = array[2, array[2, float]]
 
-proc `+`*(a, b:Vector2) :Vector2 =
+proc `+`*(a, b:Vector2): Vector2 =
   result.x = a.x + b.x
   result.y = a.y + b.y
 
 proc `+=`*(a: var Vector2, b: Vector2) =
   a = a+b
 
-proc `-`*(a, b:Vector2) :Vector2 =
+proc `-`*(a, b:Vector2): Vector2 =
   result.x = a.x - b.x
   result.y = a.y - b.y
 
-proc `*`*(a, b:Vector2) :Vector2 =
+proc `*`*(a, b:Vector2): Vector2 =
   result.x = a.x * b.x
   result.y = a.y * b.y
 
-proc `*`*(a:Vector2, b:float) :Vector2 =
+proc `*`*(a:Vector2, b:float): Vector2 =
   result.x = a.x * b
   result.y = a.y * b
 
-proc `*`*(a:float, b:Vector2) :Vector2 =
-  result = b*a
+proc `*`*(a:float, b:Vector2): Vector2 =
+  b*a
 
-proc dot*(a, b:Vector2) :float =
+proc `/`*(a:Vector2, b:float): Vector2 =
+  result.x = a.x / b
+  result.y = a.y / b
+
+proc dot*(a, b:Vector2): float =
   (a.x * b.x) + (a.y * b.y)
 
-proc length*(a:Vector2) :float =
+proc length*(a:Vector2): float =
   sqrt(a.x*a.x + a.y*a.y)
 
-proc normalize*(a:Vector2) :Vector2 =
+proc normalize*(a:Vector2): Vector2 =
   let length = a.length
   if (length != 0):
     result.x = a.x / length
     result.y = a.y / length
 
-proc vec2*(x, y:float) :Vector2 =
-  result = Vector2(x:x, y:y)
+proc vec2*(x, y:float): Vector2 =
+  Vector2(x:x, y:y)
 
-proc `*`*(a:Matrix2x2, b:Matrix2x2) :Matrix2x2 =
+proc `*`*(a:Matrix2x2, b:Matrix2x2): Matrix2x2 =
   for i in 0..1:
     for j in 0..1:
       for n in 0..1:
         result[i][j] += a[i][n] * b[n][j]
 
-proc `*`*(a:Matrix2x2, b:Vector2) :Vector2 =
-  result = vec2(a[0][0] * b.x + a[0][1] * b.y,
-                a[1][0] * b.x + a[1][1] * b.y)
+proc `*`*(a:Matrix2x2, b:Vector2): Vector2 =
+  vec2(a[0][0] * b.x + a[0][1] * b.y,
+       a[1][0] * b.x + a[1][1] * b.y)
+
+proc lerp* (a, b: auto, ratio: float): auto =
+  a*(1-ratio) + b*ratio
+
+proc lerp* (a, b: auto, ratio, dt: float): auto =
+  a*(pow(ratio, dt)) + b*(1-pow(ratio, dt))
 
 proc matrixFromAngle*(a: float): Matrix2x2 =
   let
@@ -62,23 +72,23 @@ proc matrixFromAngle*(a: float): Matrix2x2 =
             [-sinA, cosA]]
 
 proc angleFromDirection*(dir: Vector2): float =
-  result = arctan2(dir.x, dir.y)
+  arctan2(dir.x, dir.y)
 
 proc directionFromAngle*(a: float): Vector2 =
-  result = vec2(sin(a), cos(a))
+  vec2(sin(a), cos(a))
 
 proc matrixFromDirection*(dir: Vector2): Matrix2x2 =
-  result = [[ dir.y, dir.x],
-            [-dir.x, dir.y]]
+  [[ dir.y, dir.x],
+   [-dir.x, dir.y]]
 
 proc col*(r, g, b: float, a = 1.0): Color =
-  result = Color(r:r, g:g, b:b, a:a)
+  Color(r:r, g:g, b:b, a:a)
 
 proc glMatrix*(self: Matrix2x2): array[16, float] =
-  result = [self[0][0], self[1][0], 0, 0,
-            self[0][1], self[1][1], 0, 0,
-            0         , 0         , 1, 0,
-            0         , 0         , 0, 1]
+  [self[0][0], self[1][0], 0, 0,
+   self[0][1], self[1][1], 0, 0,
+   0         , 0         , 1, 0,
+   0         , 0         , 0, 1]
 
 proc radToDeg*(angle: float): float =
-  result = angle * 360 / (2 * Pi)
+  angle * 360 / (2 * Pi)
