b832b95fdbd8d7fe48e5fcdc867fe99b126f77de
woflox <eric.billingsley.1@gmail.com>
2015-03-17 21:19:21 -0400

    more weapon stuff

--- src/entity/weapon.nim
+++ src/entity/weapon.nim
@@ -9,14 +9,23 @@ type
     projectile
     blast
     obstruction
+  WeaponDeathType{.pure.} = enum
+    timed
+    probability
+  WeaponSpawnType{.pure.} = enum
+    none
+    death
+    probability
   WeaponEffect = ref object
     effectType: WeaponEffectType
     directions: seq[Vector2]
     speed: float
+    spawnEffect: WeaponEffect
+
   projectile = ref object of Entity
   blast = ref object of Entity
   obstruction = ref object of Entity
 
   Weapon* = ref object
     fireType* : FireType
-    fireDirections* : seq[Vector2]
+    effect* : WeaponEffect
