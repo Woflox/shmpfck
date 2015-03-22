54eca4085e74d28d03d04d757551a395cce8b233
woflox <eric.billingsley.1@gmail.com>
2015-03-22 14:27:55 -0400

    Reduced NN update rate

--- src/ai/neuralnet.nim
+++ src/ai/neuralnet.nim
@@ -5,8 +5,8 @@ const
   activationThreshold = 0.15
   numNeurons = 50
   numSynapsesPerNeuron = 8
-  updateRate = 1 / 300.0
-  maxUpdates = 10
+  updateRate = 1 / 60.0
+  maxUpdates = 4
 
 type
   Neuron* = object
